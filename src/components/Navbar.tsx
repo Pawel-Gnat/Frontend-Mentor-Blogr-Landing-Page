69040c27ec01c407873e729623f7c35cfc398d08
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2023-03-11 18:16:54 +0100

    Bug fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,54 +1,52 @@
-import { menuItems } from '../menuItems'
-import MenuItems from './MenuItems'
-import Anchor from './Anchor'
-import '../styles/navbar.scss'
-import { useContext, useEffect, useRef } from 'react'
-import { NavContext } from '../context/nav-context'
+import { menuItems } from '../menuItems';
+import MenuItems from './MenuItems';
+import Anchor from './Anchor';
+import '../styles/navbar.scss';
+import { useContext, useEffect, useRef } from 'react';
+import { NavContext } from '../context/nav-context';
 
 const Navbar = () => {
-	const navbarRef = useRef<HTMLDivElement>(null)
-	const ctxNav = useContext(NavContext)
+	const navbarRef = useRef<HTMLDivElement>(null);
+	const ctxNav = useContext(NavContext);
 
 	useEffect(() => {
 		const handler = (e: MouseEvent | TouchEvent) => {
-			if (ctxNav.isNavOpen && navbarRef.current && !(navbarRef.current as HTMLElement).contains(e.target as Node)) {
-				ctxNav.setIsNavOpen(prev => !ctxNav.isNavOpen)
-			}
-		}
+			const target = e.target as HTMLElement;
+			console.log(target);
 
-		document.addEventListener('mousedown', handler)
-		document.addEventListener('touchstart', handler)
+			if (
+				ctxNav.isNavOpen &&
+				navbarRef.current &&
+				!target.matches('button.btn-hamburger') &&
+				!(navbarRef.current as HTMLElement).contains(e.target as Node)
+			) {
+				ctxNav.setIsNavOpen((prev) => !prev);
+			}
+		};
+
+		document.addEventListener('mousedown', handler);
+		document.addEventListener('touchstart', handler);
 
 		return () => {
-			document.removeEventListener('mousedown', handler)
-			document.removeEventListener('touchstart', handler)
-		}
-	}, [ctxNav.isNavOpen])
+			document.removeEventListener('mousedown', handler);
+			document.removeEventListener('touchstart', handler);
+		};
+	}, [ctxNav.isNavOpen]);
 
 	return (
 		<div
 			className={`navbar ${ctxNav.isNavOpen ? ' active' : ''}`}
-			ref={navbarRef}>
+			ref={navbarRef}
+		>
 			<ul className='navbar__ul-list'>
 				{menuItems.map((menu, index) => {
-					return (
-						<MenuItems
-							items={menu}
-							key={index}
-						/>
-					)
+					return <MenuItems items={menu} key={index} />;
 				})}
 			</ul>
-			<Anchor
-				type='btn--login'
-				text='Login'
-			/>
-			<Anchor
-				type='btn--signup'
-				text='Sign Up'
-			/>
+			<Anchor type='btn--login' text='Login' />
+			<Anchor type='btn--signup' text='Sign Up' />
 		</div>
-	)
-}
+	);
+};
 
-export default Navbar
+export default Navbar;
